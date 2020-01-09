de0c5f924c6b15bde0f425593593d5f2aca8c36a
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2020-01-10 01:22:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 
 const AuthController = require('./controllers/auth')
